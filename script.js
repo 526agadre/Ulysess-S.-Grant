57843962db546d3eac0e2e14b97659651490653b
526agadre <526agadre@frhsd.com>
2025-03-02 16:12:25 -0500

    minor changes to the code

--- script.js
+++ script.js
@@ -39,21 +39,3 @@ function closeMedia() {
     const mediaDisplay = document.getElementById("mediaDisplay");
     mediaDisplay.classList.add("hidden");
 }
-/*
-// Get all accordion buttons
-var acc = document.getElementsByClassName("accordion");
-
-// Add click event listener for each accordion button
-for (var i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function() {
-        var panel = this.nextElementSibling; // Get the panel associated with the clicked button
-       
-        // Toggle the panel display (open or close)
-        if (panel.style.display === "block") {
-            panel.style.display = "none";  // Close the panel
-        } else {
-            panel.style.display = "block";  // Open the panel
-        }
-    });
-}
-    */
